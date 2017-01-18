6e8d6ea36716818e515ddafd439e3d1412d1714d
Damien Clarke <dxinteractive@gmail.com>
2017-01-18 13:35:29 +1100

    Tests for nulls

--- src/records/ChartData-test.js
+++ src/records/ChartData-test.js
@@ -35,6 +35,33 @@ const rows = [
     }
 ];
 
+const rowsWithNulls = [
+    {
+        day: 1,
+        supply: 34,
+        demand: 99,
+        fruit: "apple"
+    },
+    {
+        day: 2,
+        supply: 32,
+        demand: 88,
+        fruit: "apple"
+    },
+    {
+        day: 3,
+        supply: null,
+        demand: null,
+        fruit: "orange"
+    },
+    {
+        day: 3,
+        supply: null,
+        demand: 77,
+        fruit: "peach"
+    }
+];
+
 const columns = [
     {
         key: 'day',
@@ -54,6 +81,10 @@ const columns = [
     }
 ];
 
+//
+// constructor
+//
+
 test('ChartData Record can be created', tt => {
     const data = new ChartData(rows, columns);
     tt.true(is(data.rows, fromJS(rows)), 'rows are set correctly');
@@ -66,29 +97,68 @@ test('ChartData contents can take immutable rows and columns', tt => {
     tt.true(is(data.columns, fromJS(columns)), 'columns are set correctly');
 });
 
+//
+// methods
+//
+
+// min
+
 test('ChartData.min returns the minimum value for a column', tt => {
     const data = new ChartData(rows, columns);
     tt.is(data.min('supply'), 12, 'minimum value of a column is returned');
 });
+
+test('ChartData.min returns the minimum value for a column, even with null values', tt => {
+    const data = new ChartData(rowsWithNulls, columns);
+    tt.is(data.min('supply'), 32, 'minimum value of a column is returned');
+});
+
+// max
 
 test('ChartData.max returns the maximum value for a column', tt => {
     const data = new ChartData(rows, columns);
     tt.is(data.max('day'), 19, 'maximum value of a column is returned');
 });
 
+test('ChartData.max returns the maximum value for a column, even with null values', tt => {
+    const data = new ChartData(rowsWithNulls, columns);
+    tt.is(data.max('supply'), 34, 'maximum value of a column is returned');
+});
+
+// sum
+
 test('ChartData.sum returns the sum of values of a column', tt => {
     const data = new ChartData(rows, columns);
     tt.is(data.sum('demand'), 302, 'sum of values of a column is returned');
 });
+
+test('ChartData.sum returns the sum of values of a column, even with null values', tt => {
+    const data = new ChartData(rowsWithNulls, columns);
+    tt.is(data.sum('demand'), 77 + 88 + 99, 'sum of values of a column is returned');
+});
+
+// average
 
 test('ChartData.average returns the average of values of a column', tt => {
     const data = new ChartData(rows, columns);
     tt.is(data.average('supply'), 22.6, 'average of values of a column is returned');
 });
 
+test('ChartData.average returns the average of values of a column, even with null values', tt => {
+    const data = new ChartData(rowsWithNulls, columns);
+    tt.is(data.average('supply'), 33, 'average of values of a column is returned');
+});
+
+// median
+
 test('ChartData.median returns the median of values of a column', tt => {
     const data = new ChartData(rows, columns);
     tt.is(data.median('demand'), 56, 'median of values of a column is returned');
+});
+
+test('ChartData.median returns the median of values of a column, even with null values', tt => {
+    const data = new ChartData(rowsWithNulls, columns);
+    tt.is(data.median('demand'), 88, 'median of values of a column is returned');
 });
 
 
