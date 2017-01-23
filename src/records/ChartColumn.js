1d75c0205259b3ed0b05224d677a3b6385d07337
Damien Clarke <dxinteractive@gmail.com>
2017-01-23 11:40:36 +1100

    Move column jsdoc and flow types to column file

--- src/records/ChartColumn.js
+++ src/records/ChartColumn.js
@@ -2,10 +2,33 @@
 
 import {Record} from 'immutable';
 
-class ChartColumn extends Record({
+class ChartColumnRecord extends Record({
     key: "",
     label: "",
     isContinuous: false
 }) {}
 
-export default ChartColumn;
+export default ChartColumnRecord;
+
+/**
+ * An object to define a column for a chart. Once passed into a `ChartData` constructor these are replaced with equivalent `ChartColumn`s. Also note that you'll rarely need to set `isContinuous` explicitly as it will be inferred from data automatically when not provided.
+ *
+ * @typedef ChartColumnDefinition
+ * @type {Object|Map}
+ * @property {string} key A unique string name to refer to this column in code.
+ * @property {string} label A human readable label for this column that can be rendered by charts
+ * @property {boolean} [isContinuous] A boolean indicating if the data in this column is continuous i.e. the data has intrinsic order (like numbers). This property is optional and will be inferred from the data when not provided.
+ */
+
+/**
+ * A `ChartColumn` Immutable Record containing column-specific metadata.
+ *
+ * @typedef ChartColumn
+ * @type {Record}
+ * @property {string} key A unique string name to refer to this column in code.
+ * @property {string} label A human readable label for this column that can be rendered by charts
+ * @property {boolean} isContinuous A boolean indicating if the data in this column is continuous i.e. the data has intrinsic order (like numbers).
+ */
+
+export type ChartColumnDefinition = Object|Map<string,*>;
+export type ChartColumn = *; // todo: should be a Column Record
--- src/records/ChartData.js
+++ src/records/ChartData.js
@@ -17,31 +17,10 @@ import {
 } from 'immutable';
 
 import Column from './ChartColumn';
+import type {ChartColumnDefinition, ChartColumn} from './ChartColumn';
 
-type ChartColumnDefinition = Object|Map<string,*>;
 type ChartScalar = string|number|null;
 type ChartRow = Map<string,ChartScalar>;
-type ChartColumn = *; // todo: should be a Column Record
-
-/**
- * An object to define a column for a chart. Once passed into a `ChartData` constructor these are replaced with equivalent `ChartColumn`s. Also note that you'll rarely need to set `isContinuous` explicitly as it will be inferred from data automatically when not provided.
- *
- * @typedef ChartColumnDefinition
- * @type {Object|Map}
- * @property {string} key A unique string name to refer to this column in code.
- * @property {string} label A human readable label for this column that can be rendered by charts
- * @property {boolean} [isContinuous] A boolean indicating if the data in this column is continuous i.e. the data has intrinsic order (like numbers). This property is optional and will be inferred from the data when not provided.
- */
-
-/**
- * A `ChartColumn` Immutable Record containing column-specific metadata.
- *
- * @typedef ChartColumn
- * @type {Record}
- * @property {string} key A unique string name to refer to this column in code.
- * @property {string} label A human readable label for this column that can be rendered by charts
- * @property {boolean} isContinuous A boolean indicating if the data in this column is continuous i.e. the data has intrinsic order (like numbers).
- */
 
 /**
  * A valid chart value, which can only accept data of type `string`, `number` and `null`.
