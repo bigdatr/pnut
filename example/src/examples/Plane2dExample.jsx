f80a973868050e35927cecc6bc4a0043448a5d1c
Allan Hortle <allanhortle@gmail.com>
2017-02-23 17:10:20 +1100

    Update axis example

--- example/src/examples/Plane2dExample.jsx
+++ example/src/examples/Plane2dExample.jsx
@@ -2,215 +2,7 @@ import {Line, ChartData, Chart, Scatter, Column, Axis, Gridlines} from 'pnut';
 import React from 'react';
 import {ElementQueryHock} from 'stampy';
 
-const columns = [
-    {
-        key: 'supply',
-        label: 'Supply',
-        isContinuous: true
-    },
-    {
-        key: 'demand',
-        label: 'Demand',
-        isContinuous: true
-    },
-    {
-        key: 'month',
-        label: 'Month',
-        isContinuous: false
-    },
-    {
-        key: 'randomDemand',
-        label: 'Random Demand',
-        isContinuous: true
-    },
-    {
-        key: 'benchmark',
-        label: 'Bench Mark'
-    }
-];
-
-const rows = [
-    {
-        month: "2014-01-01",
-        supply: 123605,
-        demand: 28
-    },
-    {
-        month: "2014-02-01",
-        supply: 457959,
-        demand: 72
-    },
-    {
-        month: "2014-03-01",
-        supply: 543558,
-        demand: 96
-    },
-    {
-        month: "2014-04-01",
-        supply: 657625,
-        demand: 107
-    },
-    {
-        month: "2014-05-01",
-        supply: 724687,
-        demand: 116
-    },
-    {
-        month: "2014-06-01",
-        supply: 577673,
-        demand: 93
-    },
-    {
-        month: "2014-07-01",
-        supply: 510476,
-        demand: 85
-    },
-    {
-        month: "2014-08-01",
-        supply: 587977,
-        demand: 104
-    },
-    {
-        month: "2014-09-01",
-        supply: 589351,
-        demand: 121
-    },
-    {
-        month: "2014-10-01",
-        supply: 557710,
-        demand: 138
-    },
-    {
-        month: "2014-11-01",
-        supply: 550750,
-        demand: 139
-    },
-    {
-        month: "2014-12-01",
-        supply: 240661,
-        demand: 95
-    },
-    {
-        month: "2015-01-01",
-        supply: 278804,
-        demand: 87
-    },
-    {
-        month: "2015-02-01",
-        supply: 785962,
-        demand: 141
-    },
-    {
-        month: "2015-03-01",
-        supply: 713841,
-        demand: 129
-    },
-    {
-        month: "2015-04-01",
-        supply: 681580,
-        demand: 132
-    },
-    {
-        month: "2015-05-01",
-        supply: 930395,
-        demand: 139
-    },
-    {
-        month: "2015-06-01",
-        supply: 937566,
-        demand: 109
-    },
-    {
-        month: "2015-07-01",
-        supply: 1011621,
-        demand: 126
-    },
-    {
-        month: "2015-08-01",
-        supply: 1638135,
-        demand: 154
-    },
-    {
-        month: "2015-09-01",
-        supply: 1209174,
-        demand: 138
-    },
-    {
-        month: "2015-10-01",
-        supply: 1060541,
-        demand: 137
-    },
-    {
-        month: "2015-11-01",
-        supply: 1236615,
-        demand: 170
-    },
-    {
-        month: "2015-12-01",
-        supply: 629503,
-        demand: 125
-    },
-    {
-        month: "2016-01-01",
-        supply: 678891,
-        demand: 109
-    },
-    {
-        month: "2016-02-01",
-        supply: 1681174,
-        demand: 163
-    },
-    {
-        month: "2016-03-01",
-        supply: 1209983,
-        demand: 140
-    },
-    {
-        month: "2016-04-01",
-        supply: 1380393,
-        demand: 149
-    },
-    {
-        month: "2016-05-01",
-        supply: 1267107,
-        demand: 151
-    },
-    {
-        month: "2016-06-01",
-        supply: 1371218,
-        demand: 154
-    },
-    {
-        month: "2016-07-01",
-        supply: 1652395,
-        demand: 160
-    },
-    {
-        month: "2016-08-01",
-        supply: 1561521,
-        demand: 181
-    },
-    {
-        month: "2016-09-01",
-        supply: 1896226,
-        demand: 218
-    },
-    {
-        month: "2016-10-01",
-        supply: 1810362,
-        demand: 227
-    },
-    {
-        month: "2016-11-01",
-        supply: 1877047,
-        demand: 247
-    },
-    {
-        month: "2016-12-01",
-        supply: 770154,
-        demand: 204
-    }
-];
+import lineData from '../data/lineData';
 
 const lightLine = (props: LineProps): React.Element<any> => {
     const {coordinates} = props;
@@ -219,11 +11,7 @@ const lightLine = (props: LineProps): React.Element<any> => {
 
 class Plane2dExample extends React.Component {
     render() {
-        const chartData = (new ChartData(rows, columns)).mapRows(ii => {
-            return ii
-                .set('randomDemand', ii.get('demand') + ((Math.random() - 0.5) * 1000000))
-                .set('benchmark', 1300000);
-        });
+        const chartData = lineData;
 
         const chartProps = {
             width: 800,
@@ -258,29 +46,20 @@ class Plane2dExample extends React.Component {
                     <Line yColumn="supply" pathProps={{stroke: 'red', strokeWidth: 2}}/>
                 </Chart>
 
-                <Chart {...chartProps} stroke="#aaa">
-
-                    <Chart>
-                        <Gridlines yColumn="supply" lineHorizontal={lightLine} lineVertical={lightLine}/>
+                <Chart {...chartProps} xColumn="month">
+                    <Axis position='bottom' dimension="x"/>
+                    <Chart yColumn="supply">
+                        <Axis position='left' dimension="y"/>
+                        <Line />
+                        <Scatter />
+                        <Column />
                     </Chart>
-
-
-                    <Chart>
-                        <Gridlines yColumn="demand" lineHorizontal={lightLine} lineVertical={lightLine}/>
+                    <Chart yColumn="demand">
+                        <Axis position='right' dimension="y"/>
+                        <Scatter />
+                        <Column />
+                        <Line />
                     </Chart>
-
-                    <Chart stroke="#aaa">
-                        <Axis position='left' dimension="y" yColumn="supply"/>
-                        <Line yColumn="supply" pathProps={{stroke: 'red', strokeWidth: 2}}/>
-                    </Chart>
-
-                    <Chart stroke="#aaa">
-                        <Axis position='right' dimension="y" yColumn="demand"/>
-                        <Line yColumn="demand" pathProps={{stroke: 'blue', strokeWidth: 2}}/>
-                    </Chart>
-
-                    <Axis ticks={xScaleTicks} position='bottom' dimension="x"/>
-                    <Axis ticks={xScaleTicks} position='top' dimension="x"/>
                 </Chart>
             </div>
         </div>
