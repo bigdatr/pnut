f16da42b53a22375a1a5a0217dfcbca6c594ce3d
Allan Hortle <allanhortle@gmail.com>
2019-03-28 08:39:00 +1100

    docs: port Scatter

--- src/component/canvas/ScatterRenderable.jsx
+++ src/component/canvas/ScatterRenderable.jsx
@@ -1,41 +1,9 @@
 // @flow
-
 import PropTypes from 'prop-types';
 import type {Node} from 'react';
 import React from 'react';
 
 
-/**
- *
- * @typedef Dot
- * @type ReactElement
- *
- * @prop {Object} dotProps
- * A mixture of default props and those passed through `Scatter.props.dotProps`. This object should
- * be able to be spread onto the svg element eg. `<circle {...props.columnProps}/>`.
- *
- * @prop {number} dotProps.cx
- * The pixel x coordinate of the dot's centre
- *
- * @prop {number} dotProps.cy
- * The pixel y coordinate of dot's centre
- *
- * @prop {Object} dimensions
- * An object containing the `x` and `y` dimension values for this chart. As well as any other
- * dimensions defined when setting up the chart.
- *
- * @prop {number} index
- * The index for this row
- *
- * @prop {ChartData} data
- * The `ChartData` object that is being used to for this chart.
- *
- * @prop {Map} rowData
- * An Immutable map of the raw data for this row.
- *
- * @prop {Object[]} scaledData
- * The full array of pre scaled data.
- */
 
 const defaultDot = (props: Object): Node => {
     return <circle fill='black' r={3} {...props.dotProps}/>;
@@ -43,61 +11,18 @@ const defaultDot = (props: Object): Node => {
 
 const isNumber = (value) => typeof value === 'number' && !isNaN(value);
 
-/**
- *
- * @component
- *
- * ScatterRenderable is the low-level svg renderer for Scatter charts.
- *
- * @example
- *
- * const scaledData = [
- *     {x: 1, y: 200},
- *     {x: 2, y: 400},
- *     {x: 3, y: 600},
- *     {x: 4, y: 800},
- * ];
- *
- * return <ScatterRenderable
- *     scaledData={scaledData}
- *     dotProps={{
- *          strokeWidth: '2'
- *     }}
- *     dot={(props) => <circle {...props.columnProps} stroke="red"/>}
- * />;
- *
- *
- */
-
 export class ScatterRenderable extends React.PureComponent<*> {
     static defaultProps = {
         dot: defaultDot
     };
 
     static propTypes = {
-
-        /**
-         * {ChartData} The `ChartData` Record used to contain the data for the chart.
-         */
         data: PropTypes.object,
-
-        /**
-         * {Object} The pre-scaled data that is used to render points
-         */
         scaledData: PropTypes.arrayOf(PropTypes.shape({
             x: PropTypes.number,
             y: PropTypes.number
         })).isRequired,
-
-        /**
-         * An object of props that will be spread onto the svg element used to display dots on the chart.
-         */
         dotProps: PropTypes.object,
-
-        /**
-         * {Dot} An optional react component that will be used to render dots on the chart.
-         * Defaults to rendering a `<circle/>`.
-         */
         dot: PropTypes.func
     };
 
@@ -132,34 +57,11 @@ export class ScatterRenderable extends React.PureComponent<*> {
     }
 }
 
-/**
- *
- * @component
- *
- * Component used to render scatter/point charts. This component requires further props to define
- * what pieces of data it uses. @see `Chart` for details.
- * @name Scatter
- *
- * @example
- * <Scatter
- *     dot={(props) => <circle {...props.dotProps} fill='blue'/>}
- * />
- *
- */
-
 export default class Scatter extends React.Component<*> {
     static chartType = 'canvas';
 
     static propTypes = {
-        /**
-         * An object of props that will be spread onto the svg element used to display dots on the chart.
-         */
         dotProps: PropTypes.object,
-
-        /**
-         * {Dot} An optional react component that will be used to render dots on the chart.
-         * Defaults to rendering a `<circle/>`.
-         */
         dot: PropTypes.func
     };
 
@@ -167,3 +69,4 @@ export default class Scatter extends React.Component<*> {
         return <ScatterRenderable {...this.props} />;
     }
 }
+
