acbabc17343cd2a2b1e72aa970ad59922a1d1ff7
Allan Hortle <allanhortle@gmail.com>
2019-04-18 13:37:51 +1000

    test: useScales

--- src/__test__/useScales-test.js
+++ src/__test__/useScales-test.js
@@ -16,12 +16,97 @@ const rows = [
 
 const data = new ChartData(rows, columns);
 
-test('useScales will scale data', () => {
-    const [x, y] = useScales([
-        {columns: ['foo'], upperBound: 100},
-        {columns: ['bar'], upperBound: 100}
-    ])(data);
+describe('useScales', () => {
+    it('can create many dimensions', () => {
+        const dimensions = useScales([
+            {columns: ['foo'], range: [0, 100]},
+            {columns: ['bar'], range: [0, 100]},
+        ])(data);
 
-    expect(x.scaledData).toEqual([[25], [50], [75], [100]]);
-    expect(y.scaledData).toBe(0);
+        expect(dimensions.length).toBe(2);
+    });
+
+    it('can scale one column', () => {
+        const [foo] = useScales([
+            {columns: ['foo'], range: [0, 100]},
+        ])(data);
+
+        expect(foo.scaledData).toEqual([[[25], [50], [75], [100]]]);
+    });
+
+    it('can scale multiple columns', () => {
+        const [foo] = useScales([
+            {columns: ['foo', 'bar'], range: [0, 14]},
+        ])(data);
+
+        expect(foo.scaledData).toEqual([
+            [[2],[4],[6],[8]],
+            [[8],[10],[12],[14]]
+        ]);
+    });
+
+    it('will ignore nulls', () => {
+            const rows = [
+                {foo: 1, bar: 1},
+                {foo: null, bar: null},
+                {foo: 3, bar: 3}
+            ];
+
+            const data = new ChartData(rows, columns);
+            const [foo] = useScales([
+                {columns: ['foo', 'bar'], range: [0, 6]},
+            ])(data);
+
+            expect(foo.scaledData).toEqual([
+                [[2], [null], [6]],
+                [[2], [null], [6]]
+            ]);
+    });
+
+
+    it('should throw if you try and stack non-numerical data', () => {
+        const badRequest = () => useScales([
+            {columns: ['qux', 'qux'], range: [0, 1], stack: true}
+        ])(data);
+
+        expect(badRequest).toThrow();
+    });
+});
+
+describe('stacking', () => {
+    it('can stack multiple columns', () => {
+            const rows = [
+                {foo: 1, bar: 1},
+                {foo: 2, bar: 2},
+                {foo: 3, bar: 3}
+            ];
+
+            const data = new ChartData(rows, columns);
+            const [foo] = useScales([
+                {columns: ['foo', 'bar'], range: [0, 6], stack: true},
+            ])(data);
+
+            expect(foo.scaledData).toEqual([
+                [[0, 1], [0, 2], [0, 3]],
+                [[1, 2], [2, 4], [3, 6]]
+            ]);
+    });
+
+    it('will ignore nulls', () => {
+            const rows = [
+                {foo: 1, bar: 1},
+                {foo: null, bar: null},
+                {foo: 3, bar: 3}
+            ];
+
+            const data = new ChartData(rows, columns);
+            const [foo] = useScales([
+                {columns: ['foo', 'bar'], range: [0, 6], stack: true},
+            ])(data);
+
+            expect(foo.scaledData).toEqual([
+                [[0, 1], [null, null], [0, 3]],
+                [[1, 2], [null, null], [3, 6]]
+            ]);
+    });
 });
