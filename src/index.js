be269ac58b299c755c490b66b8a11afd67ef4713
Allan Hortle <allanhortle@gmail.com>
2019-05-21 17:36:28 +1000

    break: change index exports

--- src/index.js
+++ src/index.js
@@ -4,23 +4,8 @@ export {default as Svg} from './component/Svg';
 export {default as Chart} from './component/Chart';
 
 // Children
-export {default as Axis} from './component/axis/Axis';
-export {AxisRenderable} from './component/axis/Axis';
-export {default as Benchmark} from './component/canvas/BenchmarkRenderable';
-export {BenchmarkRenderable} from './component/canvas/BenchmarkRenderable';
-export {default as Column} from './component/canvas/ColumnRenderable';
-export {ColumnRenderable} from './component/canvas/ColumnRenderable';
-export {default as Bar} from './component/canvas/BarRenderable';
-export {BarRenderable} from './component/canvas/BarRenderable';
-export {default as Gridlines} from './component/canvas/Gridlines';
-export {GridlinesRenderable} from './component/canvas/Gridlines';
-export {default as Line} from './component/canvas/LineRenderable';
-export {LineRenderable} from './component/canvas/LineRenderable';
-export {default as Area} from './component/canvas/AreaRenderable';
-export {AreaRenderable} from './component/canvas/AreaRenderable';
-export {default as Scatter} from './component/canvas/ScatterRenderable';
-export {ScatterRenderable} from './component/canvas/ScatterRenderable';
-export {default as Histogram} from './component/canvas/HistogramRenderable';
-export {HistogramRenderable} from './component/canvas/HistogramRenderable';
-export {default as Label} from './component/canvas/LabelRenderable';
-export {LabelRenderable} from './component/canvas/LabelRenderable';
+export {default as Axis} from './component/canvas/Axis'
+export {default as Gridline} from './component/canvas/Gridline'
+export {default as Line} from './component/canvas/Line'
+export {default as Scatter} from './component/canvas/Scatter'
+export {default as Column} from './component/canvas/Column'
