6516c7c2cdf4ad4345aa76e28e4a81ee55f0b7e7
Allan Hortle <allanhortle@gmail.com>
2019-06-17 13:54:40 +1000

    build: fix linting

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ export {default as Svg} from './component/Svg';
 export {default as Chart} from './component/Chart';
 
 // Children
-export {default as Axis} from './component/canvas/Axis'
-export {default as Gridline} from './component/canvas/Gridline'
-export {default as Line} from './component/canvas/Line'
-export {default as Scatter} from './component/canvas/Scatter'
-export {default as Column} from './component/canvas/Column'
+export {default as Axis} from './component/canvas/Axis';
+export {default as Gridline} from './component/canvas/Gridline';
+export {default as Line} from './component/canvas/Line';
+export {default as Scatter} from './component/canvas/Scatter';
+export {default as Column} from './component/canvas/Column';
